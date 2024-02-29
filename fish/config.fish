8d4407ac5aeacb0ea8978bfb1db704140c8296ce
callume <edwards.callumr@gmail.com>
2024-02-29 20:54:38 +0000

    updated zellij, fish and shell gpt

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,26 @@ export LD_LIBRARY_PATH='/usr/local/cuda/lib64:/usr/local/cuda/include:/usr/local
 alias clip 'xclip -selection clipboard'
 
 set -gx COLORTERM truecolor  # For helix editor
+
+alias g='git'
+
+
+### Auto start zellij ###
+# if status is-interactive
+#     # Configure auto-attach/exit to your likings (default is off).
+#     # set ZELLIJ_AUTO_ATTACH true
+#     # set ZELLIJ_AUTO_EXIT true
+#     eval (zellij setup --generate-auto-start fish | string collect)
+# end
+if not set -q ZELLIJ
+    if test "$ZELLIJ_AUTO_ATTACH" = "true"
+        zellij attach -c
+    else
+        zellij
+    end
+
+    if test "$ZELLIJ_AUTO_EXIT" = "true"
+        kill $fish_pid
+    end
+end
+source secret_env_vars.fish
